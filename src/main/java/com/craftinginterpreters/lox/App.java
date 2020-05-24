5415f5c6dbe748eb04c3fa949b28e0b48e2928d8
Will <william.broach@gmail.com>
2020-05-23 20:13:40 -0400

    added project build

---
+++ src/main/java/com/craftinginterpreters/lox/App.java
@@ -0,0 +1,13 @@
+package com.craftinginterpreters.lox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
