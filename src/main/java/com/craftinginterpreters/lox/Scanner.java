eda01a53a2b8e74af35f52be0f674ea651a7762b
Will <william.broach@gmail.com>
2020-05-25 20:13:01 -0400

    Added Scanner.java

---
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -0,0 +1,30 @@
+package com.craftinginterpreters.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.craftinginterpreters.lox.TokenType.*;
+
+class Scanner {
+
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1; 
+    
+    Scanner(String source) {
+	this.source = source; 
+    }
+
+    List<Token> scanTokens() {
+	return null; // PASS
+    }
+
+    private boolean isAtEnd() {
+	return current >= source.length(); 
+    }
+
+}
