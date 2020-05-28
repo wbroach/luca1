f42c99e0d9c35dff9f4c1355108caffd687676a6
Will <william.broach@gmail.com>
2020-05-27 20:56:20 -0400

    in Scanner.java: refactored advance() for clarity & added match()

--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -42,23 +42,30 @@ class Scanner {
 	    case '+': addToken(PLUS); break;           
 	    case ';': addToken(SEMICOLON); break;      
 	    case '*': addToken(STAR); break;
+	    case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;      
+	    case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;    
+	    case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;      
+	    case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
 	    default:
 		Lox.error(line, "Unexpected character.");
 		break; 
 	}
     }
 
-    List<Token> scanTokens() {
-	return null; // PASS
-    }
+    private boolean match(char expected) {                 
+	if (isAtEnd()) { return false; }
+	if (source.charAt(current) != expected) { return false; } 
+	
+	current++;                                           
+	return true;                                         
+    }  
 
     private boolean isAtEnd() {
 	return current >= source.length(); 
     }
 
     private char advance() {
-	current++;
-	return source.charAt(current - 1); 
+	return source.charAt(current++); 
     }
 
     private void addToken(TokenType type) {                
