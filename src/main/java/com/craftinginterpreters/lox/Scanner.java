d7a51adcfa4151d0cc6ee262098db62bf888dbf8
Will <william.broach@gmail.com>
2020-05-28 18:46:28 -0400

    completed scanner, some pom.xml updates

--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -1,14 +1,36 @@
 package com.craftinginterpreters.lox;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.HashMap;
 
 import static com.craftinginterpreters.lox.TokenType.*;
 
 class Scanner {
 
+    private static final Map<String, TokenType> keywords;
+
+    static {
+	keywords = new HashMap<>();                        
+	keywords.put("and", AND);                       
+	keywords.put("class", CLASS);                     
+	keywords.put("else", ELSE);                      
+	keywords.put("false", FALSE);                     
+	keywords.put("for", FOR);                       
+	keywords.put("fun", FUN);                       
+	keywords.put("if", IF);                        
+	keywords.put("nil", NIL);                       
+	keywords.put("or", OR);                        
+	keywords.put("print", PRINT);                     
+	keywords.put("return", RETURN);                    
+	keywords.put("super", SUPER);                     
+	keywords.put("this", THIS);                      
+	keywords.put("true", TRUE);                      
+	keywords.put("var", VAR);                       
+	keywords.put("while", WHILE);
+    }
+    
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
     private int start = 0;   // tracks the beginning of the current lexeme
@@ -67,9 +89,37 @@ class Scanner {
 		break;
 	    case '"': string(); break;
 	    default:
-		Lox.error(line, "Unexpected character.");
+		if (isDigit(c)) { number(); }
+		else if (isAlpha(c)) {
+		    identifier(); 
+		}
+		else {
+		    Lox.error(line, "Unexpected character.");
+		}
 		break; 
 	}
+    }
+
+    private void identifier() {
+	while (isAlphaNumeric(peek())) { advance(); }
+
+	String text = source.substring(start, current);
+
+	TokenType type = keywords.get(text);
+	if (type == null) { type = IDENTIFIER; }
+	
+	addToken(type); 
+    }
+
+    private void number() {
+	while (isDigit(peek())) { advance(); }
+
+	if (peek() == '.' && isDigit(peekNext())) {
+	    advance();
+ 	    while (isDigit(peek())) { advance(); }
+	}
+
+	addToken(NUMBER, Double.parseDouble(source.substring(start, current))); 
     }
 
     private void string() {
@@ -102,6 +152,29 @@ class Scanner {
 	else {
 	    return source.charAt(current); 
 	}
+    }
+
+    private char peekNext() {
+	if (current + 1 >= source.length()) {
+	    return '\0';
+	}
+	else {
+	    return source.charAt(current + 1); 
+	}
+    }
+
+    private boolean isAlpha(char c) {
+	return (c >= 'a' && c <= 'z')      
+	       || (c >= 'A' && c <= 'Z')     
+	       || (c == '_');
+    }
+
+    private boolean isAlphaNumeric(char c) {
+	return isAlpha(c) || isDigit(c); 
+    }
+    
+    private boolean isDigit(char c) {
+	return c >= '0' && c <= '9'; 
     }
 
     private boolean isAtEnd() {
--- src/main/java/com/craftinginterpreters/lox/Token.java
+++ src/main/java/com/craftinginterpreters/lox/Token.java
@@ -14,7 +14,7 @@ class Token {
   }                                                               
 
   public String toString() {                                      
-    return type + " " + lexeme + " " + literal;                   
+    return "Type: " + type + " Lexeme: " + lexeme + " Literal: " + literal;                   
   }
     
 } 
