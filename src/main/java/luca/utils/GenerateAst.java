47240f663aa8d0a6afcd0a69b59342d5a8ff06fc
Will <william.broach@gmail.com>
2020-05-31 12:48:57 -0400

    work on GenerateAst.java

--- src/main/java/luca/utils/GenerateAst.java
+++ src/main/java/luca/utils/GenerateAst.java
@@ -5,12 +5,15 @@ import java.io.PrintWriter;
 import java.util.Arrays;                                           
 import java.util.List;                                             
 
-public class GenerateAst {                                         
+/** A class used to automate AST generation  */
+public class GenerateAst {
+    
     public static void main(String[] args) throws IOException {      
 	if (args.length != 1) {                                        
 	    System.err.println("Usage: generate_ast <output directory>");
 	    System.exit(64);                                             
 	}                                                              
 	String outputDir = args[0];                                    
-    }                                                                
+    }
+    
 } 
