2bbc299fef70c63e4a69764293f4d29eb0c231ec
Will <william.broach@gmail.com>
2020-06-14 17:58:38 -0400

    added comments to GenerateAst.java

--- scripts/GenerateExpr.sh
+++ scripts/GenerateExpr.sh
@@ -1,5 +1,5 @@
+# NOTE: This should be run from the main project directory!
+
 javac src/main/java/luca/utils/GenerateAst.java
-
 java -cp src/main/java luca.utils.GenerateAst src/main/java/luca
-
 rm src/main/java/luca/utils/GenerateAst.class
