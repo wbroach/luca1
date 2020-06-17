cb0dd1d2555492feb89dfd0a80c64404066cd010
Will <william.broach@gmail.com>
2020-06-17 18:00:44 -0400

    modified generate expression shell file

--- scripts/GenerateExpr.sh
+++ scripts/GenerateExpr.sh
@@ -1,5 +1,7 @@
 # NOTE: This should be run from the main project directory!
+AST_PATH=src/main/java/luca
+
 
 javac src/main/java/luca/utils/GenerateAst.java
-java -cp src/main/java luca.utils.GenerateAst src/main/java/luca
+java -cp src/main/java luca.utils.GenerateAst $AST_PATH
 rm src/main/java/luca/utils/GenerateAst.class
