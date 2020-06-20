ebbd8395631f5c244c5420c2c1feab35ecf522ef
Will <william.broach@gmail.com>
2020-06-20 18:15:42 -0400

    additions to Parser.java

--- src/main/java/luca/Parser.java
+++ src/main/java/luca/Parser.java
@@ -12,4 +12,20 @@ class Parser {
     Parser(List<Token> tokens) {
 	this.token = tokens; 
     }
+
+    private Expr expression() {
+	return equality(); 
+    }
+
+    private Expr equality() {                         
+	Expr expr = comparison();                       
+	
+	while (match(BANG_EQUAL, EQUAL_EQUAL)) {        
+	    Token operator = previous();                  
+	    Expr right = comparison();                    
+	    expr = new Expr.Binary(expr, operator, right);
+	}                                               
+	
+	return expr;                                    
+    }
 }
