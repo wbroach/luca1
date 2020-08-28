906197df700790c9e153acec22ae01a04ef5dcb2
Will <william.broach@gmail.com>
2020-08-28 18:37:11 -0400

    add error when expecting expression

--- src/main/java/luca/Parser.java
+++ src/main/java/luca/Parser.java
@@ -91,6 +91,8 @@ class Parser {
 	    consume(RIGHT_PAREN, "Expect ')' after expression.");
 	    return new Expr.Grouping(expr);
 	}
+
+	throw error(peek(), "Expect expression.");
     }
 
     private boolean match(TokenType... types) {
