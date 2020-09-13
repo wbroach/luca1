d073a95a92378dc99bc0b21025f0acb68cc43f13
Will <william.broach@gmail.com>
2020-09-13 16:14:31 -0400

    added to Interpreter.java

--- src/main/java/luca/Interpreter.java
+++ src/main/java/luca/Interpreter.java
@@ -8,6 +8,26 @@ class Interpreter implements Expr.Visitor<Object> {
     }
 
     @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+	Object right = evaluate(expr.right);
+
+	switch (expr.operator.type) {
+	    case BANG:
+		return !isTruthy(right);
+	    case MINUS:
+		return -((double) right);
+	}
+
+	return null;
+    }
+
+    private boolean isTruthy(Object obj) {
+	if (obj == null) { return false; }
+	else if (obj instanceof Boolean) { return (boolean) obj; }
+	else { return true; }
+    }
+
+    @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
 	return evaluate(expr.expression);
     }
@@ -15,5 +35,5 @@ class Interpreter implements Expr.Visitor<Object> {
     private Object evaluate(Expr expr) {
 	return expr.accept(this);
     }
-    
+
 }
