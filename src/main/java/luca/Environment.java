86a0a32b2b2f8bff3b802b88ef880e12a0f59ac9
Will <william.broach@gmail.com>
2020-11-10 21:54:59 -0500

    added Environment.java

---
+++ src/main/java/luca/Environment.java
@@ -0,0 +1,19 @@
+package luca;
+
+import java.util.Map;
+import java.util.HashMap;
+
+class Environment {
+
+    private final Map<String,Object> values = new HashMap<>();
+
+    void define(String name, Object value) {
+	values.put(name, value);
+    }
+
+    Object get(Token name) {
+	if (values.containsKey(name.lexeme)) { return values.get(name.lexeme); }
+
+	throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'."); 
+    }
+}
