ee6d31a7e2b4a6baa546d07244d4c82d3a787bcd
wbroach <william.broach@gmail.com>
2021-04-23 21:05:52 -0400

    added clock native function

--- src/main/java/luca/Interpreter.java
+++ src/main/java/luca/Interpreter.java
@@ -4,8 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+    final Environment globals = new Environment();
+    private Environment environment = globals;
 
-    private Environment environment = new Environment();
+    Interpreter() {
+	globals.define("clock", new LucaCallable() {
+		@Override
+		public int arity() { return 0; }
+
+		@Override
+		public Object call(Interpreter interpreter, List<Object> arguments) {
+		    return ((double) (System.currentTimeMillis() / 1000.0));
+		}
+
+		@Override
+		public Strintg toString() {
+		    return "<native fn>";
+		}
+	    });
+    }
     
     void interpret(List<Stmt> statements) {
 	try {
