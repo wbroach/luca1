3b7f8f163a557c5aa75c2a78a51e03fd49ca571f
wbroach <william.broach@gmail.com>
2021-05-24 21:44:47 -0400

    adding methods to Resolver.java

--- src/main/java/luca/Resolver.java
+++ src/main/java/luca/Resolver.java
@@ -12,4 +12,26 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     Resolver(Interpreter interpreter) {
 	this.interpreter = interpreter;
     }
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+	beginScope();
+	resolve(stmt.statements);
+	endScope();
+	return null;
+    }
+
+    void resolve(List<Statement> statements) {
+	for (Stmt statement : statements) {
+	    resolve(statement);
+	}
+    }
+
+    private void resolve(Stmt stmt) {
+	stmt.accept(this);
+    }
+
+    private void resolve(Expr expr) {
+	expr.accept(this);
+    }
 }
